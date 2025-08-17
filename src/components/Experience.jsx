fba44a259c0ae84aa0e2340b9cf703bc0d897b24
entremotivator <104661532+entremotivator@users.noreply.github.com>
2025-08-16 17:26:37 -0700

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,24 +1,38 @@
 import { RigidBody } from "@react-three/rapier";
-import { Platform} from "./Platform";
+import { Platform } from "./Platform";
 import { CharacterController } from "./CharacterController/CharacterController";
 import { Manager } from "../hooks/useYuka";
 import { JacobNpc } from "./Npcs";
 import { YukaMesh } from "./Yuka/YukaMesh";
 
 export const Experience = ({ heroRef }) => {
+  // Define clones with different positions
+  const jacobClones = [
+    { name: "Jacob1", position: [0, 0, 8] },
+    { name: "Jacob2", position: [3, 0, 5] },
+    { name: "Jacob3", position: [-4, 0, 6] },
+    { name: "Jacob4", position: [2, 0, -3] },
+  ];
+
   return (
     <>
       <ambientLight intensity={2} />
       <directionalLight position={[0, 20, 20]} intensity={1} />
+
       {/* YUKA manager */}
       <Manager>
-        {/* Hero character controller component */}
+        {/* Hero character controller */}
         <CharacterController reference={heroRef} />
-        {/* Convai NPC Jacob component */}
-        <JacobNpc name="Jacob" position={[0, 0, 8]} heroRef={heroRef} />
+
+        {/* Multiple cloned Jacob NPCs */}
+        {jacobClones.map((npc, i) => (
+          <JacobNpc key={i} name={npc.name} position={npc.position} heroRef={heroRef} />
+        ))}
+
         {/* <YukaMesh /> */}
+
         <group>
-          {/* Rapier triangler collider for stage */}
+          {/* Rapier triangle collider for stage */}
           <RigidBody friction={2} colliders="trimesh" type="fixed">
             <Platform />
           </RigidBody>
